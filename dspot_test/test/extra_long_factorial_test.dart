fbc4413129ae9b7e93f33f7caa83fe80e2997cac
Yunior Rodriguez <47259501+yunior123@users.noreply.github.com>
2022-05-09 08:36:48 -0400

    extra long factorial

---
+++ dspot_test/test/extra_long_factorial_test.dart
@@ -0,0 +1,11 @@
+import 'package:dspot_test/extra_long_factorial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('extra long factorial ...', () async {
+    extraLongFactorials(5);
+    // 120
+    extraLongFactorials(25);
+    //
+  });
+}
